9519e81df8d9f5cff3c5f4f3f42b177509b6863a
Abbas Parpia <aparpia03@gmail.com>
2020-02-15 21:06:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+
+ def prime?(int)
+    if int <= 1 
+      return false 
+     else
+      (2..int-1).collect do |number|
+      if int % number == 0
+        return false
+        end
+      end
+    end 
+    true 
+  end
+  
+    
+    
