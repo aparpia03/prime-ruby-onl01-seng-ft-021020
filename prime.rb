2be996a279a54df911854d30a677eb4901045615
Abbas Parpia <aparpia03@gmail.com>
2020-02-15 20:06:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,9 @@
 # Add  code here!
 
 def prime?(int)
-  
-
-number_array.push(int) 
-number_array.select do |int|
     if int % int == 0
        true
     elsif int % int == 1  
       false 
     end 
-  end
 end  
